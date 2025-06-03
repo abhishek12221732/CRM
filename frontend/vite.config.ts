0cf1a8f527779a05051e801f1101fea9158290de
Abhishek Kumar <abhishek.kumar.2301.vivek@gmail.com>
2025-06-03 18:04:13 +0530

    adding ports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,5 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+   server: {
+    port: 3000, 
+    host: '0.0.0.0' // Allow external access on Render
+  }
+
   
 })
