fdcfc886f3aa6eab6c6241f795bb27c1a4da1922
Abhishek Kumar <abhishek.kumar.2301.vivek@gmail.com>
2025-06-03 18:30:59 +0530

    refixing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,10 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-   server: {
-    port: 3000, 
-    host: 'crm-frontend-sltp.onrender.com' // Allow external access on Render
-  }
+
+
 
   
 })
