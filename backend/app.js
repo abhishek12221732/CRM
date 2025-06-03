849577a62944a3a3c5204ffcfa879897c1e52b81
Abhishek Kumar <abhishek.kumar.2301.vivek@gmail.com>
2025-06-03 17:36:19 +0530

    adding origins

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173', '*'],
+  origin: ['http://localhost:5173','https://crm-frontend-sltp.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
