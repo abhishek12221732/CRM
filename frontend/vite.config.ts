f9f0f5a4f448ba29262f136c28ee93d316d40065
Abhishek Kumar <abhishek.kumar.2301.vivek@gmail.com>
2025-06-03 18:09:04 +0530

    fixing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss(),],
    server: {
     port: 3000, 
-    host: '0.0.0.0' // Allow external access on Render
+    host: 'crm-frontend-sltp.onrender.com' // Allow external access on Render
   }
 
   
